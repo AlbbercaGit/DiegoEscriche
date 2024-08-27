466d660fdc775f7382c56d44eef89bd76673e4fa
AlbbercaGit <alejandroalbbercabarcelona@gmail.com>
2024-08-27 16:07:22 +0200

    Galeria de discos

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Discos from './components/Discos';
+import Galeria from './components/Galeria';
 import Footer from './components/Footer';
+import { ReactLenis, useLenis } from 'lenis/react'
+
+function Layout() {
+  const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  })
+
+
+
+}
 
 function App() {
   return (
     <div className="App">
+          <ReactLenis root>
       <Header />
       <Hero />
       <Discos />
+      <Galeria />
       <Footer />
+      </ReactLenis>
     </div>
   );
 }
---
+++ src/components/Galeria.js
@@ -0,0 +1,120 @@
+import { motion, useTransform, useScroll } from "framer-motion";
+import React, { useRef } from "react";
+
+function Footer() {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({ target: targetRef });
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-100%"]);
+  return (
+    <main ref={targetRef} className="relative h-[200vh]">
+      <div className="sticky w-full top-0 flex h-[100vh] items-center  overflow-hidden">
+        
+        <motion.div
+          style={{ x }}
+          className="flex gap-4 items-center justify-center"
+        >
+          <div className=" relative h-[400px] w-[400px] flex justify-center items-center">
+            <img
+              alt="Diego de espaldas"
+              className="absolute inset-0 h-full    shadow-lg"
+              src="https://raw.githubusercontent.com/AlbbercaGit/videos/5206f38db9655f68e3a2261efe8dab3aed4ba60c/escama.jpg
+"
+            />
+
+            <iframe
+              src="https://open.spotify.com/embed/track/2vlCD96gbODeKevuusi8Ic?utm_source=generator"
+              width="60%"
+              height="352"
+              frameBorder="0"
+              allowFullScreen
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+              className="relative z-10"
+            ></iframe>
+          </div>
+          {/* Fantastic Explosion */}
+          <div className=" relative h-[400px] w-[400px] flex justify-center items-center">
+            <img
+              alt="Diego de espaldas"
+              className="absolute inset-0 h-full    shadow-lg"
+              src="https://raw.githubusercontent.com/AlbbercaGit/videos/main/fantastic.jfif
+"
+            />
+            <iframe
+              src="https://open.spotify.com/embed/track/2sLSdMlwnt48B2YAOqNHpu?utm_source=generator"
+              width="60%"
+              height="352"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+              className="relative z-10"
+            ></iframe>
+          </div>
+          {/* Futuro Terror */}
+          <div className=" relative h-[400px] w-[400px] flex justify-center items-center">
+            <img
+              alt="Diego de espaldas"
+              className="absolute inset-0 h-full    shadow-lg"
+              src="https://raw.githubusercontent.com/AlbbercaGit/videos/main/presente.jfif
+"
+            />
+
+            <iframe
+              src="https://open.spotify.com/embed/track/4SJ9LQDLIVjxtBtZPQquoJ?utm_source=generator"
+              width="60%"
+              height="352"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+              className="relative z-10"
+            ></iframe>
+          </div>
+          {/* Mausoleo */}
+          <div className=" relative h-[400px] w-[400px] flex justify-center items-center">
+            <img
+              alt="Diego de espaldas"
+              className="absolute inset-0 h-full    shadow-lg"
+              src="https://raw.githubusercontent.com/AlbbercaGit/videos/main/a0251967785_65.jfif
+"
+            />
+
+            <iframe
+              src="https://open.spotify.com/embed/track/55splxCKVjk24tQK1TUKiT?utm_source=generator&theme=0"
+              width="60%"
+              height="352"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+              className="relative z-10"
+            ></iframe>
+          </div>
+          {/* Santa Companha */}
+          <div className=" relative h-[400px] w-[400px] flex justify-center items-center">
+            <img
+              alt="Diego de espaldas"
+              className="absolute inset-0 h-full    shadow-lg"
+              src="https://raw.githubusercontent.com/AlbbercaGit/videos/main/santacompanha.jpg
+"
+            />
+
+            <iframe
+              src="https://open.spotify.com/embed/track/5bPZhQMnFkOqJwnyk8Xfwi?utm_source=generator"
+              width="60%"
+              height="352"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+              className="relative z-10"
+            ></iframe>
+          </div>
+        </motion.div>
+      </div>
+    </main>
+  );
+}
+
+export default Footer;
